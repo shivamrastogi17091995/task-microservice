2e2b421e92daf9cfa70bd1da90bb567be7052fff
Shivam Rastogi <shivamrastogi17091995@gmail.com>
2025-04-03 09:08:43 +0530

    Added DB

---
+++ db/database.go
@@ -0,0 +1,20 @@
+package db
+
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	dsn := os.Getenv("DATABASE_URL")
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+}
--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 	"os"
 	"task-microservice/config"
+	"task-microservice/db"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.LoadEnv()
+	db.InitDB()
 	router := gin.Default()
 	port := os.Getenv("PORT")
 	log.Println("Server running on port", port)
